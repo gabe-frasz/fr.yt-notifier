3a4dbb5f59a41b70f2aff046c1c9c9dd92db5d9f
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-12-30 18:54:32 -0300

    fix: get channel id with prefix

--- src/utils/extract-info-from-xml.ts
+++ src/utils/extract-info-from-xml.ts
@@ -1,20 +1,20 @@
 const regexes = {
-  videoId: /<yt:videoId>(.*?)<\/yt:videoId>/,
-  title: /<title>(.*?)<\/title>/g,
-  link: /<link rel="alternate" href="(.*?)"/,
-  author: /<name>(.*?)<\/name>/,
-  channelId: /<yt:channelId>(.*?)<\/yt:channelId>/,
+	videoId: /<yt:videoId>(.*?)<\/yt:videoId>/,
+	title: /<title>(.*?)<\/title>/g,
+	link: /<link rel="alternate" href="(.*?)"/,
+	author: /<name>(.*?)<\/name>/,
+	channelId: /<yt:channelId>(.*?)<\/yt:channelId>/g,
 };
 
 export function extractInfoFromXML(xml: string) {
 	const id = xml.match(regexes.videoId)?.[1];
-  if (!id) return null;
+	if (!id) return null;
 
 	return {
 		id,
 		title: Array.from(xml.matchAll(regexes.title))[1][1] ?? "New video",
 		link: xml.match(regexes.link)?.[1] ?? "Unknown",
 		author: xml.match(regexes.author)?.[1] ?? "Unknown",
-		channelId: xml.match(regexes.channelId)?.[1] ?? "Unknown",
+		channelId: Array.from(xml.matchAll(regexes.channelId))[1][1] ?? "Unknown",
 	};
 }
