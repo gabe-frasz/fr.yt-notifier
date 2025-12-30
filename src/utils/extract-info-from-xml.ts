e9aec43767d1528f6718f535c8721a0008ce28d2
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2025-12-30 19:03:58 -0300

    fix: revert matchAll and verify if channel id starts with UC prefix

--- src/utils/extract-info-from-xml.ts
+++ src/utils/extract-info-from-xml.ts
@@ -1,20 +1,25 @@
 const regexes = {
-	videoId: /<yt:videoId>(.*?)<\/yt:videoId>/,
-	title: /<title>(.*?)<\/title>/g,
-	link: /<link rel="alternate" href="(.*?)"/,
-	author: /<name>(.*?)<\/name>/,
-	channelId: /<yt:channelId>(.*?)<\/yt:channelId>/g,
+  videoId: /<yt:videoId>(.*?)<\/yt:videoId>/,
+  title: /<title>(.*?)<\/title>/g,
+  link: /<link rel="alternate" href="(.*?)"/,
+  author: /<name>(.*?)<\/name>/,
+  channelId: /<yt:channelId>(.*?)<\/yt:channelId>/,
 };
 
 export function extractInfoFromXML(xml: string) {
 	const id = xml.match(regexes.videoId)?.[1];
-	if (!id) return null;
+  if (!id) return null;
+
+  const title = Array.from(xml.matchAll(regexes.title))[1][1] ?? "New video";
+	const link = xml.match(regexes.link)?.[1] ?? "Unknown";
+	const author = xml.match(regexes.author)?.[1] ?? "Unknown";
+  const channelId = xml.match(regexes.channelId)?.[1] ?? "Unknown";
 
 	return {
 		id,
-		title: Array.from(xml.matchAll(regexes.title))[1][1] ?? "New video",
-		link: xml.match(regexes.link)?.[1] ?? "Unknown",
-		author: xml.match(regexes.author)?.[1] ?? "Unknown",
-		channelId: Array.from(xml.matchAll(regexes.channelId))[1][1] ?? "Unknown",
+    title,
+		link,
+		author,
+		channelId: channelId.startsWith("UC") ? channelId : `UC${channelId}`,
 	};
 }
