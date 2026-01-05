9d7f933d3c9d64c8f7fb1b27ed98ad8320b7d20b
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2026-01-05 20:06:23 -0300

    fix: redirect unverified user to login page

--- src/middlewares/verify-token.ts
+++ src/middlewares/verify-token.ts
@@ -5,12 +5,12 @@ import { factory } from "../libs";
 
 export const verifyToken = factory.createMiddleware(async (c, next) => {
   const token = getCookie(c, "auth_token");
-  if (!token) return c.text("Unauthorized", 401);
+  if (!token) return c.redirect("/admin/login");
 
   try {
     await verify(token, c.env.JWT_SECRET);
   } catch (e) {
-    return c.text("Unauthorized", 401);
+    return c.redirect("/admin/login");
   }
 
   return next();
