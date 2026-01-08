c4e95479ac42f57458c5a3aa7d0dce41e1dbb9c7
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2026-01-07 21:48:49 -0300

    feat: log important steps

--- src/schedules/fetch-latest-videos-from-rss.ts
+++ src/schedules/fetch-latest-videos-from-rss.ts
@@ -1,6 +1,10 @@
 import type { Env } from "../env";
 import { TypedKV } from "../libs";
-import { fetchLatestVideoFromChannel, isDamnShort, sendMobileAlert } from "../utils";
+import {
+	fetchLatestVideoFromChannel,
+	isDamnShort,
+	sendMobileAlert,
+} from "../utils";
 
 export async function fetchLatestVideosFromRSS(env: Env) {
 	const kv = new TypedKV(env.KV);
@@ -12,15 +16,26 @@ export async function fetchLatestVideosFromRSS(env: Env) {
 		channels.map((channel) => fetchLatestVideoFromChannel(env, channel.id)),
 	);
 
+	console.log(
+		`[fetchLatestVideosFromRSS] Found ${videos.length} videos:`,
+		JSON.stringify(videos, null, 2),
+	);
+
 	for (const video of videos) {
 		if (!video) continue;
 
 		const channel = await kv.getChannel(video.channelId);
 		if (!channel || video.id === channel.lastVideoId) continue;
 
+		console.log(
+			`[fetchLatestVideosFromRSS] New video for channel ${video.channelId}: ${video.title}`,
+		);
+
 		await kv.updateChannelLastVideoId(video.channelId, video.id);
 
-    if (await isDamnShort(video.id)) continue;
+		if (await isDamnShort(video.id)) continue;
+
+		console.log("[fetchLatestVideosFromRSS] Not a short. Sending mobile alert");
 
 		await sendMobileAlert({
 			video: {
